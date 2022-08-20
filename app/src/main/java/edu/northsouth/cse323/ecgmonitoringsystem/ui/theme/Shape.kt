c4783b65fdeae00a3dfcb59cec2b5c9c01016afd
Tahmid A. Khan <tahmid.khan1@northsouth.edu>
2022-08-20 22:28:44 +0600

    Initial commit: scaffold an Android app using Android Studio
    * Use template: Empty Compose Activity.
    * Convert Gradle files from Groovy to KTS.
    * Remove the `release` build type.

---
+++ app/src/main/java/edu/northsouth/cse323/ecgmonitoringsystem/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package edu.northsouth.cse323.ecgmonitoringsystem.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
